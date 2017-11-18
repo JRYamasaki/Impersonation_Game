877e33f8a2359d8953be2f9cb7f499c5ba605876
JRYamasaki <yamasakijeffrey@gmail.com>
2017-11-18 12:52:50 -0800

    Add username back to scores so tests can run properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114221650) do
+ActiveRecord::Schema.define(version: 20171118200904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171114221650) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "username"
   end
 
   create_table "users", force: :cascade do |t|
--- test/models/score_test.rb
+++ test/models/score_test.rb
@@ -4,4 +4,9 @@ class ScoreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "testtest" do
+  	score = Score.new
+  	assert_not score.save
+  end
 end
