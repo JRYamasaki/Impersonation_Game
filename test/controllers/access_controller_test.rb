da45c65d2b664620b84e938767a08085e3bf564b
JRYamasaki <yamasakijeffrey@gmail.com>
2017-11-18 14:03:52 -0800

    Add tests for login credentials

--- test/controllers/access_controller_test.rb
+++ test/controllers/access_controller_test.rb
@@ -5,4 +5,14 @@ class AccessControllerTest < ActionDispatch::IntegrationTest
     get access_login_url
     assert_response :success
   end
+
+  test "should display error message" do
+  	post access_attempt_login_url, params:{:username => "notAUser", :password => "notAUser"}
+  	assert_equal "Incorrect username/password", flash[:notice]
+  end
+
+  test "should correctly login user" do
+  	post access_attempt_login_url, params:{"username" => "MyString", "password" => "MyString"}
+  	assert_equal "Logged in succesfully", flash[:notice]
+  end
 end
