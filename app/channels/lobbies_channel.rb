dbc1629b18e85871f8b442de131065ab19728e16
Richard <rgodard@sfu.ca>
2017-11-28 13:45:08 -0800

    removed a channel that wasn't needed

--- app/channels/lobbies_channel.rb
+++ app/channels/lobbies_channel.rb
@@ -1,16 +1,3 @@
 class LobbiesChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "lobbies_#{params['lobby_id']}_channel"
-  end
-
-  def unsubscribed
-    #Stuff to do when one unsubscribes from a channel
-  end
-
-  def send_response(data)
-    #Will take in the response and possibly store into a database for further processing
-
-    #Message implementation defined here
-    current_user.messages.create!(body: data['message'], lobby_id: data['lobby_id'])
-  end
+#not needed
 end
