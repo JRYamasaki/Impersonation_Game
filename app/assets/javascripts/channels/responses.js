ad7059eb234caa1c28264132fee625cd8f7f024b
Richard <rgodard@sfu.ca>
2017-11-29 22:40:31 -0800

    response boxes now seperate

--- app/assets/javascripts/channels/responses.js
+++ app/assets/javascripts/channels/responses.js
@@ -1,11 +1,13 @@
+var counter = 0;
+
 App.responses = App.cable.subscriptions.create('ResponsesChannel', {
 	received: function(data) {
 		//$("#response_box_" + data.user).removeClass('hidden')
 		//$("#response").hide(); // Hides the response box, but for all users in lobby
 		//return $('#response_box_' + data.user).append(this.renderResponse(data));
-
-		$("#response_box").removeClass('hidden')
-		return $('#response_box').append(this.renderResponse(data));
+		counter = counter+1;
+		$("#response_box_" + counter).removeClass('hidden')
+		return $('#response_box_' + counter).append(this.renderResponse(data));
 	},
 	renderResponse: function(data) {
 		return "<p> <b>" + data.user + ": </b>" + data.response + "</p>";
